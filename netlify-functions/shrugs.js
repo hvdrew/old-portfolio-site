0a87f8d3a7b1493760e330a756181db32abd50da
Andrew <andrew@merison.io>
2019-05-30 08:49:44 -0700

    Added functions directory

---
+++ netlify-functions/shrugs.js
@@ -0,0 +1,16 @@
+import querystring from 'querystring';
+
+exports.handler = async (event, context) => {
+    if (event.httpMethod !== 'POST') {
+        return { statusCode: 405, body: 'Method Not Allowed' };
+    }
+
+    const params = querystring.parse(event.body);
+    const text = params.text;
+    const command = params.command;
+
+    return {
+        statusCode: 200,
+        body: `¯\\_('-')_/¯`
+    }
+}
